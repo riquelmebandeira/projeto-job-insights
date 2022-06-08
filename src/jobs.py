d586e4d377dab91aa61b2c11b2bf5c09ba63b925
riquelme <riquelmebandeira1@outlook.com>
2022-06-08 12:53:33 -0300

    feat: read function

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,9 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path, encoding="utf-8") as file:
+        jobs = csv.DictReader(file, delimiter=",", quotechar='"')
+
+        jobs_list = [job for job in jobs]
+
+    return jobs_list
