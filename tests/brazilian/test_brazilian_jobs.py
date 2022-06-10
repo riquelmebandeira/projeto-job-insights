65a8bbc10468f9716266b24a83b43b17bc7197cd
riquelme <riquelmebandeira1@outlook.com>
2022-06-09 22:16:26 -0300

    test: test_brazilian_jobs function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    output = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    expected = {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
+    assert output[0] == expected
